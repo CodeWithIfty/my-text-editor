24e1d348798e6a5d91ad11eb974580819e96a41b
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-08-17 16:38:03 +0600

    creat setTextAlignment functional more logical

--- js/script.js
+++ js/script.js
@@ -1,44 +1,28 @@
 document.getElementById('bold-btn').addEventListener('click', function(){
     setTextStyle('bold-btn', 'font-bold');
-})
+});
 
 document.getElementById('italic-btn').addEventListener('click', function(){
     setTextStyle('italic-btn', 'italic');
-})
+});
 
 document.getElementById('underline-btn').addEventListener('click', function(){
     setTextStyle('underline-btn', 'underline');
-})
+});
 
 const alignLeftBtn = document.getElementById('text-left');
 const alignCenterBtn = document.getElementById('text-center');
 const alignRightBtn = document.getElementById('text-right');
 
-
-    alignLeftBtn.addEventListener('click', function(){
-        const textEditor = document.getElementById('editor');
-        textEditor.classList.remove('text-center', 'text-right')
-        textEditor.classList.add('text-left')
-        alignCenterBtn.classList.remove('dark:text-white', 'dark:bg-gray-600')
-        alignRightBtn.classList.remove('dark:text-white', 'dark:bg-gray-600')
-        alignLeftBtn.classList.add('dark:text-white', 'dark:bg-gray-600')
+alignLeftBtn.addEventListener('click', function(){
+        setTextAlignment('text-left', alignLeftBtn);
         
-    })
+});
 
-    alignCenterBtn.addEventListener('click', function(){
-        const textEditor = document.getElementById('editor');
-        textEditor.classList.remove('text-left', 'text-right');
-        textEditor.classList.add('text-center');
-        alignRightBtn.classList.remove('dark:text-white', 'dark:bg-gray-600');
-        alignLeftBtn.classList.remove('dark:text-white', 'dark:bg-gray-600');
-        alignCenterBtn.classList.add('dark:text-white', 'dark:bg-gray-600');
-    })
+alignCenterBtn.addEventListener('click', function(){
+        setTextAlignment('text-center', alignCenterBtn);
+});
 
-    alignRightBtn.addEventListener('click', function(){
-        const textEditor = document.getElementById('editor');
-        textEditor.classList.remove('text-left', 'text-center')
-        textEditor.classList.add('text-right')
-        alignCenterBtn.classList.remove('dark:text-white', 'dark:bg-gray-600')
-        alignLeftBtn.classList.remove('dark:text-white', 'dark:bg-gray-600')
-        alignRightBtn.classList.add('dark:text-white', 'dark:bg-gray-600')
-    })
+alignRightBtn.addEventListener('click', function(){
+        setTextAlignment('text-right', alignRightBtn);
+});
--- js/utilities.js
+++ js/utilities.js
@@ -6,3 +6,15 @@ function setTextStyle(class1, class2){
     btn.classList.toggle('dark:bg-gray-600');
  
 }   
+
+function setTextAlignment(btnClass, activeBtn){
+    const textEditor = document.getElementById('editor');
+        textEditor.classList.remove('text-center', 'text-right');
+        textEditor.classList.add(btnClass);
+
+        alignLeftBtn.classList.remove('dark:text-white', 'dark:bg-gray-600');
+        alignCenterBtn.classList.remove('dark:text-white', 'dark:bg-gray-600');
+        alignRightBtn.classList.remove('dark:text-white', 'dark:bg-gray-600');
+        
+        activeBtn.classList.add('dark:text-white', 'dark:bg-gray-600');
+}
