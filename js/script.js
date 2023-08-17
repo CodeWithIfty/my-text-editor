34f1f41f749b71e41c7c87f947244eb9a90db67d
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-08-17 18:17:09 +0600

    added text size updating feature

--- js/script.js
+++ js/script.js
@@ -26,3 +26,37 @@ alignCenterBtn.addEventListener('click', function(){
 alignRightBtn.addEventListener('click', function(){
         setTextAlignment('text-right', alignRightBtn);
 });
+
+
+        const textarea = document.getElementById('editor');
+        const increaseSizeButton = document.getElementById('font-size-increase');
+        const inputSizeButton = document.getElementById('font-size-input');
+        const decreaseSizeButton = document.getElementById('font-size-decrease');
+        
+        let fontSize = parseFloat(inputSizeButton.value);
+
+        increaseSizeButton.addEventListener('click', function() {
+            fontSize += 2;
+            updateFontSize(fontSize);
+        });
+
+        decreaseSizeButton.addEventListener('click', function() {
+            fontSize -= 2;
+            updateFontSize(fontSize);
+        });
+
+        inputSizeButton.addEventListener('input', function() {
+            let newSize = parseFloat(inputSizeButton.value, 10);
+            if (!isNaN(newSize)) {
+                updateFontSize(newSize);
+            }
+        });
+
+        function updateFontSize(newSize) {
+            if (newSize < 10) {
+                newSize = 10; // Minimum font size
+            }
+            textarea.style.fontSize = newSize + 'px';
+            inputSizeButton.value = newSize;
+
+        }
